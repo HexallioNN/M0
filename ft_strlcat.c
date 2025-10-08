5e8d8be86ee5a1d16edd19fad4fb49c18aecc3d0
Ilter Kalach <ikalach@f1r4s17.codam.nl>
2025-10-08 14:42:16 +0200

    strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: ikalach <ikalach@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/07 17:14:49 by ikalach           #+#    #+#             */
-/*   Updated: 2025/10/07 17:20:29 by ikalach          ###   ########.fr       */
+/*   Updated: 2025/10/08 14:41:58 by ikalach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,23 @@ static int	ft_strlen(const char *s)
 	return (count);
 }
 
-size_t		strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int dstl;
-	int srcl;
+	size_t	dstl;
+	size_t	srcl;
+	size_t	i;
 
 	dstl = ft_strlen(dst);
 	srcl = ft_strlen(src);
-
+	if (size <= dstl)
+		return (size + srcl);
+	i = 0;
+	while (src[i] != '\0' && (dstl + i) < (size - 1))
+	{
+		dst[dstl + i] = src[i];
+		i++;
+	}
+	dst[dstl + i] = '\0';
 	return (dstl + srcl);
 }
 
@@ -40,8 +49,11 @@ size_t		strlcat(char *dst, const char *src, size_t size);
 
 // int	main(void)
 // {
-// 	char test[14] = "1234567";
-// 	char *test2 = "12345";
-// 	ft_strlcat(test, test2, 14);
-// 	printf("%s", test);
+// 	char	string1[20] = "This is a ";
+// 	char	*string2;
+// 	size_t	size;
+
+// 	string2 = "test 1234567890";
+// 	size = ft_strlcat(string1, string2, sizeof(string1));
+// 	printf("%s, %zu", string1, size);
 // }
