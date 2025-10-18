9b3081b45d281ba64f0f268e8d3366764bdae6c7
Ilter Kalach <ikalach@f0r1s2.codam.nl>
2025-10-18 09:40:35 +0200

    test

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,10 +6,41 @@
 /*   By: ikalach <ikalach@student.codam.nl>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 15:40:37 by ikalach           #+#    #+#             */
-/*   Updated: 2025/10/16 15:40:52 by ikalach          ###   ########.fr       */
+/*   Updated: 2025/10/18 09:38:39 by ikalach          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdio.h>
+
 char	*ft_itoa(int n)
 {
+	char	temp_result[15];
+	char	*result;
+	int		i;
+	int		temp;
+
+	result = malloc(15);
+	temp = 0;
+	i = 0;
+	while (n > 0)
+	{
+		temp = n % 10;
+		n = n / 10;
+		temp_result[i] = temp + '0';
+		i++;
+	}
+	temp = 0;
+	while (temp_result[temp] != '\0')
+	{
+		result[temp] = temp_result[i - 1];
+		i--;
+		temp++;
+	}
+	return (result);
 }
+
+// int	main(void)
+// {
+// 	printf("%s", ft_itoa(2135135));
+// }
